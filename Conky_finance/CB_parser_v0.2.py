74232d2a4fb1de47b69fde56231d754bbfc2435d
protocolNZT <kirillsez@gmail.com>
2020-04-28 23:26:33 +0300

    Update cource

--- Conky_finance/CB_parser_v0.2.py
+++ Conky_finance/CB_parser_v0.2.py
@@ -12,7 +12,7 @@ headers = {'accept': '*/*',
            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
                          'Chrome/79.0.3945.130 Safari/537.36'}
 
-base_url = 'https://www.cbr.ru/'
+base_url = 'https://www.cbr.ru/key-indicators/'
 
 
 def finance_parse(base_url, headers):
@@ -20,17 +20,18 @@ def finance_parse(base_url, headers):
     request = session.get(base_url, headers=headers)
     if request.status_code == 200:
         soup = bs(request.content, 'lxml')
-        div = soup.find('div', id="widget_exchange").text
-        title = str(textwrap.wrap(div))
+        div = soup.find('div', {'class': 'dropdown_content'})
+        title = str(textwrap.wrap(div.text))
+        print(title)
         cource_txt = (title[3:14])
-        date_daily = (title[20:30])
-        date_now = (title[31:41])
-        dollar_txt = (title[45:57])
-        price_dollar_daily = (title[68:75])
-        price_dollar_now = (title[86:94])
-        euro_txt = (title[98:104])
-        price_euro_daily = (title[115:122])
-        price_euro_now = (title[133:141])
+        date_daily = (title[29:39])
+        date_now = (title[40:50])
+        dollar_txt = (title[55:69])
+        price_dollar_daily = (title[73:80])
+        price_dollar_now = (title[81:88])
+        euro_txt = (title[93:101])
+        price_euro_daily = (title[104:111])
+        price_euro_now = (title[112:119])
 
         file = open('/home/{user}/.config/conky/Conky_NZT/Conky_finance/usdeuro.txt', 'w')     # path to file
         file.write(cource_txt + ' на ' + date_now + '\n')
