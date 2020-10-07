421d34e4db02f52a2f7b969107bbd6593cce6205
protocol <kirillsez@gmail.com>
2020-10-07 21:04:35 +0300

    Refactor CB parser

--- Conky_Weather/weather.py
+++ Conky_Weather/weather.py
@@ -6,11 +6,12 @@ import datetime
 
 times = datetime.datetime.now()
 
-API_key = ''    # API-key https://openweathermap.org/
-owm = pyowm.OWM(API_key, language='ru')
+API_key = 'eb932b5090a32c0b2e30ceb852f314fe'    # API-key https://openweathermap.org/
+owm = pyowm.OWM(API_key)
 
 sity = 'Лондон'  # you location sity
-observation = owm.weather_at_place(sity)
+# observation = owm.weather_at_place(sity)
+observation = owm.weather_manager()
 w = observation.get_weather()
 temperature = w.get_temperature('celsius')['temp']
 observation_list = owm.weather_around_coords(-22.57, -43.12)
@@ -20,11 +21,11 @@ speeed = w.get_wind()['speed']
 cloud = w.get_clouds()
 znak = w.get_weather_icon_url()
 icon_znak = urllib.request.urlopen(znak)
-file = open('/home/{user}/.config/conky/Conky_NZT/images/icons_weather.png', 'wb')
+file = open('/home/protocol/.config/conky/Conky_NZT/images/icons_weather.png', 'wb')
 file.write(icon_znak.read())
 file.close()
 
-file = open('/home/{user}/.config/conky/Conky_NZT/Conky_Weather/weather.txt', 'w')     # path to file
+file = open('/home/protocol/.config/conky/Conky_NZT/Conky_Weather/weather.txt', 'w')     # path to file
 file.write('На ' + times.strftime("%d-%m-%Y %H:%M") + '\n')
 file.write('В ' + sity + 'е наблюдается:\n')
 file.write(w.get_detailed_status() + '. Влажноcть ' + str(humidity_percentage) + '%')
--- Conky_finance/CB_parser_v0.2.py
+++ Conky_finance/CB_parser_v0.2.py
@@ -1,51 +1,97 @@
 #!/usr/bin/python3
-
 # € $
-
+# import numpy as np
+import sys
+import time
 import requests
-import re
-import textwrap
-from bs4 import BeautifulSoup as bs
+import csv
+from bs4 import BeautifulSoup
 
+print(sys.version)
+print(sys.executable)
+print(time.strftime("%d-%m-%Y %H:%M"))
+
+name_time_file = time.strftime("%d_%m_%Y")
+
+base_url_eng = 'https://www.cbr.ru/eng/key-indicators/'  # English site locale
+base_url_ru = 'https://www.cbr.ru/key-indicators/'  # Russian site locale
 
 headers = {'accept': '*/*',
-           'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                         'Chrome/79.0.3945.130 Safari/537.36'}
+           'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (HTML, like Gecko) '
+                         'Chrome/79.0.3945.130 Safari/537.36'
+           }
 
-base_url = 'https://www.cbr.ru/key-indicators/'
+files = 'usdeuro.txt'   # '/protocol/.config/conky/Conky_NZT/Conky_finance/usdeuro.txt'
 
 
-def finance_parse(base_url, headers):
+def get_html(base_url, params=None):
     session = requests.Session()
-    request = session.get(base_url, headers=headers)
-    if request.status_code == 200:
-        soup = bs(request.content, 'lxml')
-        div = soup.find('div', {'class': 'dropdown_content'})
-        title = str(textwrap.wrap(div.text))
-        cource_txt = (title[3:14])
-        date_daily = (title[29:39])
-        date_now = (title[40:50])
-        dollar_txt = (title[55:69])
-        price_dollar_daily = (title[73:80])
-        price_dollar_now = (title[81:88])
-        euro_txt = (title[93:101])
-        price_euro_daily = (title[104:111])
-        price_euro_now = (title[112:119])
+    request = session.get(base_url, headers=headers, params=params)
+    return request
 
-        file = open('/home/{user}/.config/conky/Conky_NZT/Conky_finance/usdeuro.txt', 'w')     # path to file
-        file.write(cource_txt + ' на ' + date_now + '\n')
-        file.write(dollar_txt + ' = ' + price_dollar_now + '.руб\n')
-        file.write(euro_txt + ' = ' + price_euro_now + '.руб\n')
 
-        file.write('\n' + cource_txt + ' на ' + date_daily + '\n')
-        file.write(dollar_txt + ' = ' + price_dollar_daily + '.руб\n')
-        file.write(euro_txt + ' = ' + price_euro_daily + '.руб\n')
-
-        file.close()
-
+def get_current_data(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    current_data = soup.find_all('tr')
+    if len(current_data) >= 12:
+        return len(current_data)
     else:
-        print('ERROR')
+        return len(current_data)
 
 
-if __name__ == '__main__':
-    finance_parse(base_url, headers)
+def get_content(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find('div', class_='dropdown_content')
+    global currency
+    currency = []
+    list_currency = []
+    try:
+        for item in items.find_all('div', class_='key-indicator_table_wrapper'):
+            if item.get_text():
+                list_currency.append((item.get_text().strip().split('\n', )))
+            else:
+                print(item.img.get('src'))
+    except (RuntimeError, TypeError, NameError):
+        print('Oops!  Something broke.  Try again...')
+
+    currency.append(list_currency[0][0:1] + list_currency[0][2:3])
+    currency.append(list_currency[0][7:8] + list_currency[0][12:13])
+    currency.append(list_currency[0][17:18] + list_currency[0][22:23])
+
+    currency.append(list_currency[0][0:1] + list_currency[0][1:2])
+    currency.append(list_currency[0][7:8] + list_currency[0][11:12])
+    currency.append(list_currency[0][17:18] + list_currency[0][21:22])
+
+    currency.append(list_currency[1][0:2])
+    currency.append(list_currency[1][6:8] + list_currency[1][10:11])
+    currency.append(list_currency[1][15:17] + list_currency[1][19:20])
+    currency.append(list_currency[1][24:26] + list_currency[1][28:29])
+    currency.append(list_currency[1][33:35] + list_currency[1][37:38])
+
+    return currency
+
+
+def save_data(currency, files):
+    with open(files, 'w') as txtfile:
+        writer = csv.writer(txtfile)
+        for line in currency:
+            writer.writerow(line)
+
+
+def parse_cbr():
+    html = get_html(base_url_ru)
+    if html.status_code == 200:
+        current_data = get_current_data(html.text)
+        if current_data >= 12:
+            content_list = get_content(html.text)
+            print(currency)
+            print(f'List of: {int((len(content_list))) }', 'currencies')
+            save_data(currency, files)
+            print('Data saved to file:', files)
+        else:
+            print('Oops! The list is not valid.', f'Actual:{current_data}; Expected:{14};')
+    else:
+        print('Error status code site:', html.status_code)
+
+
+parse_cbr()
