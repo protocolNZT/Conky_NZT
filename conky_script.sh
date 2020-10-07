347c5d217b0eaf2fdeacc2aa93f17700687da3bc
protocol <kirillsez@gmail.com>
2020-10-07 21:10:06 +0300

    Update conkyrc and script

--- conky_script.sh
+++ conky_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #~/.config/conky/Conky_finance/CB_parser.py
  python ~/.config/conky/Conky_NZT/Conky_finance/CB_parser_v0.2.py
- python ~/.config/conky/Conky_NZT/Conky_Weather/weather.py
+# python ~/.config/conky/Conky_NZT/Conky_Weather/weather.py
